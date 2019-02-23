cfb69de2d407672fd7259187d123a0133f75b193
onmax <maximogarciamtnez@gmail.com>
2019-02-23 19:38:23 +0100

    añadido 3º ejercicio 1º entrega

---
+++ 2º sesión/code/pregunta-3/fun_rara.m
@@ -0,0 +1,8 @@
+function yp=rara(t,y)
+    yp = y/5;
+    if ((t>1)&(t<3))
+        T=(t-1)/2; T=T*(1-T)*4; TT=1-T;
+        temp = T*cos(2*(t-1)*pi*y)/(abs(y)+0.1);
+        yp = (y/5)*TT + temp;
+    end
+return
