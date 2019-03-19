86f0fa1bf336da4eaf1a6cf80f5778d0ba4f8af6
onmax <maximogarciamtnez@gmail.com>
2019-03-19 05:06:09 +0100

    2º entrega

--- 2º sesión/code/pregunta-3/adapta12.m
+++ 2º sesión/code/pregunta-3/adapta12.m
@@ -30,7 +30,7 @@ function [T, S]=adapta12(fun,Tspan,y0,tol)
      s2 = (K1 + K2) / 2;
     
      % Calcular estimacion del error E
-     E = norm(abs(s1 - s2));
+     E = norm(s2 - s1);
 
      % Calcular ratio r entre E y tol 
      ratio = E / tol;
@@ -41,7 +41,7 @@ function [T, S]=adapta12(fun,Tspan,y0,tol)
      % Actualizar h usando la formula optima con un factor adicional 0.8
      % Comprobad que no nos salimos del intervalo en el siguiente salto
      % Recortad h si se da el caso.
-     h = (h * 0.8/ ratio^0.5);
+     h = (h*0.8 / ratio^0.5);
      
      if T(end)+h > Tfin
          h = Tfin - T(end);
