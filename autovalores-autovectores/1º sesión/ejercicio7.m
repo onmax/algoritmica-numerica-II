0b41074f627adb7f2e621d422d6c553b87cb0eac
onmax <maximogarciamtnez@gmail.com>
2019-04-11 15:01:35 +0200

    plots

--- autovalores-autovectores/1º sesión/ejercicio7.m
+++ autovalores-autovectores/1º sesión/ejercicio7.m
@@ -1,3 +1,4 @@
+
 G = [
     [0,1,0,1,1];
     [1,0,0,1,0];
@@ -26,13 +27,26 @@ end
 % Matriz de covarianza
 A = C * C';
 
-% Metodo de la potencia
+% Metodo de la potencia sobre A
 nmax = 20;
 x0 = ones(Grows, 1)/norm(ones(Grows, 1));
-[lambda1, x1] = pot_basico(G, nmax, x0);
-[lambda2, x2] = pot_basico(A, nmax, x0);
+[lambda1, x1, res1] = pot_basico(A, nmax, x0);
+
+% Metodo de la deflacion
+A2 = A - lambda1 * x1 * x1';
+
+% Metodo de la potencia sobre A2
+[lambda2, x2, res2] = pot_basico(A2, nmax, x0);
 
 
-% Vector de residuos
-res1 = A * x - lambda * x;
-    
+% Graficas
+lambda3 = 1.1;
+range=1:20;
+for k=range
+    lambdas1(k)=abs(lambda2/lambda1)^k;
+    lambdas2(k)=abs(lambda3/lambda2)^k;
+end
+subplot(1,2,1);
+plot(range,lambdas1,'b',range,res1,'r');
+subplot(1,2,2);
+plot(range, lambdas2,'b',range,res2,'r');
--- autovalores-autovectores/1º sesión/pot_basico.m
+++ autovalores-autovectores/1º sesión/pot_basico.m
@@ -1,6 +1,7 @@
-function [lambda,x]=pot_basico(A, nmax, x0)
+function [lambda,x, res1]=pot_basico(A, nmax, x0)
     % Calcula el mayor (abs) autovalor lambda de A y un autovector asociado x
     n=size(A,1);
+    res1=size(A,1);
     if nargin == 1
         x0=rand(n,1); % Vector de arranque
         nmax=100; % Numero max iteraciones
@@ -10,6 +11,6 @@ function [lambda,x]=pot_basico(A, nmax, x0)
         x1=A*x;
         lambda= x'*x1; % otra opción lambda=norm(x1, p)/norm(x, p)
         x=x1/norm(x1);
+        res1(k) = norm(A * x1 - lambda * x1);
     end
 return
- 
